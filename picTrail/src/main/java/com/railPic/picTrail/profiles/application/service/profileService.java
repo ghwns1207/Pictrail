39fa46161e3dd58c0a808bec234e15888d62b490
hoho <hojune961207@gmail.com>
2025-04-01 14:07:55 +0900

    비밀번호 변경 코드 작성, 회원 정보 수정

--- picTrail/src/main/java/com/railPic/picTrail/profiles/application/service/profileService.java
+++ picTrail/src/main/java/com/railPic/picTrail/profiles/application/service/profileService.java
@@ -1,5 +1,7 @@
 package com.railPic.picTrail.profiles.application.service;
 
+import com.railPic.picTrail.profiles.application.port.in.UpdateProfile;
+import com.railPic.picTrail.profiles.application.port.in.UpdateProfileUseCase;
 import com.railPic.picTrail.profiles.application.port.in.UpdatePwUseCase;
 import com.railPic.picTrail.util.InMemoryAuthUserRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class profileService implements UpdatePwUseCase {
+public class profileService implements UpdatePwUseCase, UpdateProfileUseCase {
 
     private final InMemoryAuthUserRepository userRepository;
 
@@ -21,6 +23,20 @@ public class profileService implements UpdatePwUseCase {
     public String updatePw(String username, String password) {
         String hashedPw = passwordEncoder.encode(password);
         log.info(hashedPw);
-        return userRepository.updatePw(username, hashedPw);
+        return userRepository.findByUsername(username)
+                .map(exUser -> {
+                    exUser.changePassword(hashedPw);
+                    return "비밀번호가 성공적으로 변경되었습니다.";
+                }).orElse("사용자를 찾을 수 없습니다.");
+    }
+
+    @Override
+    public String updateProfile(UpdateProfile updateProfile) {
+        return userRepository.findByUsername(updateProfile.username())
+                .map(exUser-> {
+                    exUser.setEmail(updateProfile.email());
+                    exUser.setUsername(updateProfile.username());
+                    return "회원 정보 수정을 성공적으로 변경되었습니다.";
+                }).orElse("사용자를 찾을 수 없습니다.");
     }
 }
--- picTrail/src/main/java/com/railPic/picTrail/util/InMemoryAuthUserRepository.java
+++ picTrail/src/main/java/com/railPic/picTrail/util/InMemoryAuthUserRepository.java
@@ -1,19 +1,16 @@
 package com.railPic.picTrail.util;
 
-<<<<<<< HEAD
+
 import com.railPic.picTrail.auth.application.port.out.UserRepository;
 import com.railPic.picTrail.auth.domain.User;
-=======
 
 import com.railPic.picTrail.auth.domain.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
->>>>>>> origin/master
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
 @Repository
-<<<<<<< HEAD
 public class InMemoryAuthUserRepository implements UserRepository {
     private final List<User> authUsers = new ArrayList<>(); // 메모리 저장소
 
@@ -65,34 +62,5 @@ public class InMemoryAuthUserRepository implements UserRepository {
 //            return "사용자를 찾을 수 없습니다.";
 //        }
 //    }
-=======
-public class InMemoryAuthUserRepository {
-    private final List<User> authUsers = new ArrayList<>(); // 메모리 저장소
-
-    public boolean addUser(User user) {
-        return authUsers.add(user);
-    }
-
-    public Optional<User> findByUsername(String username) {
-        return authUsers.stream()
-                .filter(user -> user.getUsername().equals(username))
-                .findFirst();
-    }
-
-    public List<User> findAll(){
-        return authUsers;
-    }
-
-    public String updatePw(String username, String password){
-        Optional<User> userOptional = findByUsername(username);
-        if(userOptional.isPresent()){
-            userOptional.get().setPassword(password);
-            return "비밀번호가 성공적으로 변경되었습니다.";
-        }else {
-            return "사용자를 찾을 수 없습니다.";
-        }
-    }
->>>>>>> origin/master
-
 
 }
